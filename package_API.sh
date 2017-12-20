ef61cd3fe564ebd6b39459f5b6a357ffa4faada4
Dhruv Parekh <dp358@njit.edu>
2017-12-20 18:44:23 -0500

    very final push

--- package_API.sh
+++ package_API.sh
@@ -1,18 +1,18 @@
-echo "Packaging Bundle..."
+echo "Bundling now......."
 packageNum=`./testRabbitMQClient.php apiPackage-v | xargs`
-	echo "test...."
+	#echo "test...."
 	mkdir temp/
 	apiScrDIR=$HOME/rabbitmqphp_example/
 		cp -a $apiScrDIR* temp/
 	tar -czvf apiPackage-v"$packageNum".tar.gz -C temp/ . 
 	rm -r temp
         echo `ls | grep apiPackage-v"$packageNum"`
-		echo "Bundle Complete"
+		echo "-----DONE-----"
 	# Scp package to the deploy server
-	echo "Sending to deploy server..."
+	echo "Sending to server..."
 	scp -r apiPackage-v"$packageNum".tar.gz kevv@192.168.1.13:/home/kevv/git/it490Deploy/packages/	
 	cp apiPackage-v"$packageNum".tar.gz apiPackages/
 	rm apiPackage-v"$packageNum".tar.gz
 	php updatePackage.php apiPackage-v"$packageNum".tar.gz "$packageNum"
-	echo "Packaging Complete! Sent to Deployment Server."
+	echo "--DONE--"
 
